d6657eefa00220133869d12ceec4ab3d476f94ea
Nagarjun_g <nagarjun.g@onedatasoftware.com>
2023-12-06 15:57:18 +0530

    file structured

--- lets_quiz/settings.py
+++ lets_quiz/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'letsquiz.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+    # 'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # third party apps
     'crispy_forms',
-    # 'jazzmin',
     # my apps
     'quiz',
 
